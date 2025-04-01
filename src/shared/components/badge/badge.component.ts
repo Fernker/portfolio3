4d49d7a09ee3735ba585f11957da26b5d68f42a7
Kenny F <kenny@ninety.io>
2025-04-01 14:32:18 -0600

    adds initial components

---
+++ src/shared/components/badge/badge.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'port-badge',
+  imports: [],
+  templateUrl: './badge.component.html',
+  styleUrl: './badge.component.scss'
+})
+export class BadgeComponent {
+
+}
---
+++ src/shared/components/section/section.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'port-section',
+  imports: [],
+  templateUrl: './section.component.html',
+  styleUrl: './section.component.scss'
+})
+export class SectionComponent {
+
+}
