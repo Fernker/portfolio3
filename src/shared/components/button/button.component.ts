e93415defc9c69abcd18fc1fe24e4c54a128c836
Kenny F <kenny@ninety.io>
2025-06-03 15:02:21 -0600

    fix: several tweaks

--- src/shared/components/button/button.component.ts
+++ src/shared/components/button/button.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
-  selector: 'port-button',
-  imports: [],
-  templateUrl: './button.component.html',
-  styleUrl: './button.component.scss'
+    selector: 'port-button',
+    imports: [],
+    templateUrl: './button.component.html',
+    styleUrl: './button.component.scss'
 })
 export class ButtonComponent {
-
+    href = input.required<string>();
 }
